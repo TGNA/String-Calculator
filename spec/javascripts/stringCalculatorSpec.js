ab0bfce111aca7562f2fa3e608d5d10959d6f5b4
Oscar <oscarbc@me.com>
2014-08-09 01:02:54 +0200

    red

--- spec/javascripts/stringCalculatorSpec.js
+++ spec/javascripts/stringCalculatorSpec.js
@@ -1,5 +1,25 @@
-describe("String Calculator", function() {
-	it("should behave...", function() {
-		expect(hola()).toBe("Hola");
-	});
+describe("The string calculator", function(){
+    it("accepts a string and outputs a number", function() {
+        expect(typeof calculate("")).toBe("number");
+    });
+
+    it("calculate '1+1' and returns 2", function() {
+    	expect(calculate("1+1")).toBe(2);
+    });
+
+    it("calculates '1-1' and returns 0", function() {
+      expect(calculate('1-1')).toBe(0);
+    });
+
+    it("calculates '3*3' and returns 9", function() {
+      expect(calculate('3*3')).toBe(9);
+    });
+
+    it("calculates '6/3' and returns 2", function() {
+      expect(calculate('6/3')).toBe(2);
+    });
+
+    it("calculates '60/3' and returns 20", function() {
+      expect(calculate('60/3')).toBe(20);
+    });
 });
--- stringCalculator.js
+++ stringCalculator.js
@@ -1,13 +1,38 @@
-function stringCalculator() {
-	return "Hola";
+function calculate(operation) {
+
+  var separatedOperation = operation.split('');
+
+  var firstOperand = parseInt(separatedOperation[0]);
+  var secondOperand = parseInt(separatedOperation[2]);
+  var mathOperator = separatedOperation[1];
+
+  var operators = {
+    "+": function(a, b) { return a + b },
+    "-": function(a, b) { return a - b },
+    "*": function(a, b) { return a * b },
+    "/": function(a, b) { return a / b },
+  };
+
+  if (isNaN(firstOperand) || isNaN(secondOperand)) {
+    return NaN;
+  }else{
+    return (operators[mathOperator](firstOperand, secondOperand));
+  };
 }
+
+
 
 // TO DO LIST
 
 /*
-	1. "" => "EMPTY"
-	2. " " => "EMPTY"
-	3. "1+1" => > "2"
-	4. "1/1" => "1"
-	5. "10-2" => "8"
+	1. "" => 0
+	2. " " => 0
+	3. "1+1" => 2
+	4. "1/1" => 1
+	5. "10-2" => 8
+	6. "3*2" => 6
+
+
+
+  
 */
